fd3e75c6efab4e05598c581d5bc344b684f0a9c3
Ryan Kristoffer Bartolay <ryank@peplink.com>
2019-07-12 11:09:39 +0800

    revert previous method

--- elasticsearch56/src/main/java/com/ryan/tutorials/elasticsearch56/Main.java
+++ elasticsearch56/src/main/java/com/ryan/tutorials/elasticsearch56/Main.java
@@ -23,10 +23,9 @@ import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import com.ryan.tutorials.elasticsearch56.model.Ticket;
 
-
 public class Main {
-    
-    private static String m_elasticSearchHostname = "10.8.77.20";
+
+    private static String m_elasticSearchHostname = "localhost";
     private static Integer m_elasticSearchPort = 9200;
     private static String m_elasticsearchIndex = "reqtracker_tickets";
 
@@ -53,7 +52,7 @@ public class Main {
         IndexResponse response = client.index(request);
         System.err.println("Ticket added in elasticsearch: " + response.getResult());
     }
-    
+
     public static void getSingleDocument() {
         try (RestClient restClient = RestClient
                 .builder(new HttpHost(m_elasticSearchHostname, m_elasticSearchPort, "http")).build()) {
@@ -67,111 +66,99 @@ public class Main {
             e.printStackTrace();
         }
     }
+
     public static void getMultiDocument() {
         try (RestClient restClient = RestClient
                 .builder(new HttpHost(m_elasticSearchHostname, m_elasticSearchPort, "http")).build()) {
             RestHighLevelClient client = new RestHighLevelClient(restClient);
-            
+
             SearchRequest searchRequest = new SearchRequest(m_elasticsearchIndex);
-            
+
             SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            
+
             BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
             BoolQueryBuilder roleQueryBuilder = QueryBuilders.boolQuery();
-            
+
             List<String> ticketIds = new ArrayList<>();
             ticketIds.add("9070081");
             ticketIds.add("9070082");
             ticketIds.add("9070083");
             ticketIds.add("9070084");
             ticketIds.add("9070085");
-            
-            
+
+            roleQueryBuilder.must(QueryBuilders.termsQuery("ticketId", ticketIds));
+
+            boolQueryBuilder.must(roleQueryBuilder);
+            searchSourceBuilder.query(boolQueryBuilder);
+            searchSourceBuilder.from(0);
+            searchSourceBuilder.size(5);
+
+            searchRequest.source(searchSourceBuilder);
+
+            SearchResponse response = client.search(searchRequest);
+            System.err.println(response);
+
+            SearchHit[] searchHits = response.getHits().getHits();
+            for (SearchHit hit : searchHits) {
+                System.err.println(hit.getSource());
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void getMultiDocumentWithCondition() {
+        try (RestClient restClient = RestClient
+                .builder(new HttpHost(m_elasticSearchHostname, m_elasticSearchPort, "http")).build()) {
+            RestHighLevelClient client = new RestHighLevelClient(restClient);
+
+            SearchRequest searchRequest = new SearchRequest(m_elasticsearchIndex);
+
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+            BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+            BoolQueryBuilder roleQueryBuilder = QueryBuilders.boolQuery();
+
+            List<String> ticketIds = new ArrayList<>();
+            ticketIds.add("9070081");
+            ticketIds.add("9070082");
+            ticketIds.add("9070083");
+            ticketIds.add("9070084");
+            ticketIds.add("9070085");
 
             roleQueryBuilder.must(QueryBuilders.termsQuery("ticketId", ticketIds));
 
             roleQueryBuilder.mustNot(QueryBuilders.termsQuery("ticketId", "9070083"));
 
             boolQueryBuilder.must(roleQueryBuilder);
-            searchSourceBuilder.query(boolQueryBuilder); 
-            searchSourceBuilder.from(0); 
+            searchSourceBuilder.query(boolQueryBuilder);
+            searchSourceBuilder.from(0);
             searchSourceBuilder.size(5);
-            
 
-            
             searchRequest.source(searchSourceBuilder);
-            
+
             SearchResponse response = client.search(searchRequest);
             System.err.println(response);
-            
+
             SearchHit[] searchHits = response.getHits().getHits();
             for (SearchHit hit : searchHits) {
                 System.err.println(hit.getSource());
             }
-            
-            //searchSourceBuilder.query(bqb);
+
+            // searchSourceBuilder.query(bqb);
 //            client.search(bqb);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) throws IOException {
 //        addESRecord();
         getMultiDocument();
-        
-        // TODO Auto-generated method stub
-//        String recipientTo = "12312";
-//        
-//        Pattern pattern = Pattern.compile("[\\w\\.]+\\+(\\w+)@.+");
-//        Matcher matcher = pattern.matcher(recipientTo);
-//        
-//        
-//        if (matcher.find()) {
-//            System.err.println(matcher.group(1));
-//        } else {
-//            System.err.println("nothing");
-//        }
-        
-//        String text = "RE FW  [Peplink #9060153] Testing #23131 sd22";
-//
-//        Pattern subjectPattern = Pattern.compile("\\[Peplink #\\d+].+");
-//        Pattern ticketIdPattern = Pattern.compile("\\[Peplink #\\d+]");
-//        
-//        
-//        Matcher subjectMatcher = subjectPattern.matcher(text);
-//        boolean matches = subjectMatcher.matches();
-//        System.err.println(matches);
-//
-//        if (subjectMatcher.find()) {
-//            System.err.println(subjectMatcher.start() + " ;" + subjectMatcher.end());
-//            String subject = text.substring(subjectMatcher.start(), subjectMatcher.end());
-//
-//            System.err.println("subject : " + subject);
-//            
-//            Matcher ticketIdMatcher = ticketIdPattern.matcher(subject);
-//
-//            if (ticketIdMatcher.find()) {
-//                String ticketId = subject.substring(ticketIdMatcher.start(), ticketIdMatcher.end());
-//
-//                System.err.println("ticketId : " + ticketId);
-//                
-//                String sub = subject.substring(ticketIdMatcher.end(), subject.length()).trim();
-//                
-//                System.err.println("subject clean : " + sub);
-//            }
-//            
-//        }
 
-//        String text = "[Peplink #9060153] Testing #23131 sd22";
-//        
-//        Pattern subjectPattern = Pattern.compile("Peplink #\\d+");
-//        
-//        Matcher subjectMatcher = subjectPattern.matcher(text);
-//        
-//        if(subjectMatcher.find()) {
-//            System.err.println(subjectMatcher.start() + " " + subjectMatcher.end());
-//        }
     }
 
 }
